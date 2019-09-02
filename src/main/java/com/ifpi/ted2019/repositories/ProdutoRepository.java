61b319df5968249e687a8c3eefb54adb188a25c2
Dávila Oliveira Gomes <2017118050074@ifpi.int>
2019-09-02 14:26:43 -0300

    Resolvendo a referência cíclica

--- src/main/java/com/ifpi/ted2019/repositories/ProdutoRepository.java
+++ src/main/java/com/ifpi/ted2019/repositories/ProdutoRepository.java
@@ -2,8 +2,6 @@ package com.ifpi.ted2019.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.ifpi.ted2019.domain.Categoria;
 import com.ifpi.ted2019.domain.Produto;
 
 @Repository
