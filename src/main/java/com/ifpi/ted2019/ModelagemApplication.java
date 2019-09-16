818105adde74ebd418f3330e701afe0e3f7abb87
Dávila Oliveira Gomes <2017118050074@ifpi.int>
2019-09-16 07:13:49 -0300

    Estado e Cidade implementados

--- src/main/java/com/ifpi/ted2019/ModelagemApplication.java
+++ src/main/java/com/ifpi/ted2019/ModelagemApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ifpi.ted2019.domain.Categoria;
+import com.ifpi.ted2019.domain.Cidade;
+import com.ifpi.ted2019.domain.Estado;
 import com.ifpi.ted2019.domain.Produto;
 import com.ifpi.ted2019.repositories.CategoriaRepository;
+import com.ifpi.ted2019.repositories.CidadeRepository;
+import com.ifpi.ted2019.repositories.EstadoRepository;
 import com.ifpi.ted2019.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class ModelagemApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemApplication.class, args);
@@ -32,16 +40,36 @@ public class ModelagemApplication implements CommandLineRunner {
 		Produto produto1 = new Produto(null, "Computador", 2000.00);
 		Produto produto2 = new Produto(null, "Impressora", 8000.00);
 		Produto produto3 = new Produto(null, "Mouse", 80.00);
-
+		
 		categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
 		categoria2.getProdutos().addAll(Arrays.asList(produto2));
-
+		
 		produto1.getCategorias().addAll(Arrays.asList(categoria1));
 		produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
 		produto3.getCategorias().addAll(Arrays.asList(categoria1));
+		
+		Estado estado1 = new Estado(null, "Piauí");
+		Estado estado2 = new Estado(null, "Ceará");
+		
+		Cidade cidade1 = new Cidade(null, "Fortaleza", estado1);
+		Cidade cidade2 = new Cidade(null, "Piripiri", estado2);
+		Cidade cidade3 = new Cidade(null, "Parnaíba", estado2);
+		
+		
+		estado1.getCidades().addAll(Arrays.asList(cidade2, cidade3));
+		estado2.getCidades().addAll(Arrays.asList(cidade1));
+
+		
 
 		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
 		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
+		estadoRepository.saveAll(Arrays.asList(estado1, estado2));
+		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3));
+		
+		
+
+		
+	
 
 		
 
